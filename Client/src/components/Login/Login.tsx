11104c0073bd1580e12675a0c7f4f12bb2b2b815
aswanth6000 <aswanth682@gmail.com>
2023-11-03 11:31:49 +0530

    login server side complete

--- Client/src/components/Login/Login.tsx
+++ Client/src/components/Login/Login.tsx
@@ -1,6 +1,37 @@
-
+import axios from "axios";
+import { useForm } from "../../Hooks/useForm"
 
 export default function Login() {
+  const [value, handleChange] =useForm({
+    email: '',
+    password : ''
+  })
+  console.log(value);
+  
+
+
+  const handleLogin = async(e) =>{
+    e.preventDefault()
+    try{
+      const response = await axios.post('/login', {
+        method : 'POST',
+        headers : {
+          'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify(value)
+      })
+      if (response.status === 200) {
+        console.log('Login successful');
+      } else {
+         console.log('Login failed');
+      }
+    }
+    catch(err){
+      console.log(err);
+      
+    }
+  }
+  
   return (
     <div>
       <section className="bg-gray-50 dark:bg-gray-900">
@@ -10,7 +41,7 @@ export default function Login() {
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Sign in to your account
               </h1>
-              <form className="space-y-4 md:space-y-6">
+              <form className="space-y-4 md:space-y-6" onSubmit={(handleLogin)}>
                 <div>
                   <label
                     htmlFor="email"
@@ -22,6 +53,8 @@ export default function Login() {
                     type="email"
                     name="email"
                     id="email"
+                    value={value.email}
+                    onChange={handleChange}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:outline-none "
                     placeholder="name@company.com"
                     required
@@ -39,6 +72,8 @@ export default function Login() {
                     name="password"
                     id="password"
                     placeholder="••••••••"
+                    value={value.password}
+                    onChange={handleChange}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:outline-none"
                     required
                   />
@@ -49,8 +84,7 @@ export default function Login() {
                   </a>
                 </div>
                 <button
-                  type="submit"
-                  className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                 type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   Sign in
                 </button>
